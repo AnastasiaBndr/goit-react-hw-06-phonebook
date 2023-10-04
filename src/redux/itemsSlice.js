e92d00474602630b7b4ee3c6a5902e9aeca9f07c
AnastasiaBndr <abondarchuk1149@gmail.com>
2023-10-04 21:17:41 +0300

    efscscds

--- src/redux/itemsSlice.js
+++ src/redux/itemsSlice.js
@@ -2,15 +2,6 @@ import {createSlice } from "@reduxjs/toolkit";
 import {persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 
-var elementsFromStorage=[{ id: 'id-1', name: 'Rosie Simpson', number: '+380004814000' },
-    { id: 'id-2', name: 'Hermione Kline', number: '+380018050200' },
-    { id: 'id-3', name: 'Eden Clements', number: '+382004004700' },
-    { id: 'id-4', name: 'Annie Copeland', number: '+380002023600' },
-    { id: 'id-5', name: 'Rosie Simpson', number: '+382804000500' },
-    { id: 'id-6', name: 'Hermione Kline', number: '+380020304050' },
-    { id: 'id-7', name: 'Eden Clements', number: '+380000300567' },
-    { id: 'id-8', name: 'Annie Copeland', number: '+380204050600' },]
-
 export const itemsSlice=createSlice({
     name:'items',
     initialState:{items:[]},
